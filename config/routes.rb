4b70011d76c64c43b1ccd5c059b2dad600618a8f
saimaja <saimaakhtar@gmail.com>
2020-05-27 13:02:26 -0400

    few updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
     
+    
+    root to: "application#homepage"
+    
     get 'login', to: 'sessions#show_login_form'
     post 'login', to: 'sessions#login_user'
     delete 'logout', to: 'sessions#logout_user'
+    get 'profile', to: 'posts#index'
 
     # post 'posts/:id', to: 'posts#create_comment'
- 
+    resources :users 
+      
     resources :posts do 
-      resources :zens
-      resources :comments
-    end
-    resources :users
+        resources :zens
+        resources :comments
+      end
+  
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
